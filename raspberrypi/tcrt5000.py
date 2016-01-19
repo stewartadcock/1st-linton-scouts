649aa157bfee200241ca967fbf26454b0d9c41ba
Stewart Adcock <stewart.adcock@gmail.com>
2016-01-19 21:23:30 +0000

    Add TCRT5000 demo script. This is an IR reflective proximity sensor.

---
+++ raspberrypi/tcrt5000.py
@@ -0,0 +1,32 @@
+"""tcrt5000.py - demo script for TCRT5000 GPIO circuit - an IR Proximity Sensor.
+
+See tcrt5000.md for details. 
+
+'.' is output if reflection is detected; 'X', otherwise.
+
+Press Ctrl+c to exit.
+"""
+
+import RPi.GPIO as GPIO
+import time
+
+print (__doc__)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN)
+
+try:
+  while True:
+    time.sleep(0.1)
+
+    if GPIO.input(4)==1:
+      print ('.')
+    else:
+      print ('X')
+
+except KeyboardInterrupt:
+  GPIO.cleanup()
+
+except Exception as e:
+  print (str(e))
+  GPIO.cleanup()
