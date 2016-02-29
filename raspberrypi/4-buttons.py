e810684170092b3de71ba84b5d2b36dcee7461cc
Stewart Adcock <stewartadcock@git.com>
2016-02-29 22:07:27 +0000

    Add 4 x button GPIO circuit script.

---
+++ raspberrypi/4-buttons.py
@@ -0,0 +1,32 @@
+"""4-buttons.py - a GPIO circuit with for momentary push buttons.
+
+Press Ctrl+c to exit.
+"""
+
+import RPi.GPIO as GPIO
+import time
+
+print (__doc__)
+
+signal_pins = [4, 17, 27, 22]
+pin_states = [False, False, False, False]
+
+GPIO.setmode(GPIO.BCM)
+for pin in signal_pins:
+  GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+try:
+  while True:
+    prev_pin_states = pin_states[0:4]
+    for i in range(len(signal_pins)):
+      pin_states[i] = GPIO.input(signal_pins[i]) == GPIO.LOW
+
+    if cmp(pin_states, prev_pin_states) != 0:
+      print (str(pin_states))
+
+except KeyboardInterrupt:
+  GPIO.cleanup()
+
+except Exception as e:
+  print (str(e))
+  GPIO.cleanup()
